a93c30a81605d43aba13076c3a720427472c4734
Arthur <arthur.felipe1993@gmail.com>
2025-04-05 21:55:06 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vozAmiga',
+  webDir: 'www'
+};
+
+export default config;
