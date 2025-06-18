ac544539257ead89fbee1cf2d7dee62350b3742a
Arthur <arthur.felipe1993@gmail.com>
2025-06-18 18:48:43 -0300

    Acerto após merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
-export const environment = {
-  production: true
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCd22zm_QslFWvh34i9bn2nnWwn9uKu8Hc",
+  authDomain: "vozamiga-4f62c.firebaseapp.com",
+  projectId: "vozamiga-4f62c",
+  storageBucket: "vozamiga-4f62c.firebasestorage.app",
+  messagingSenderId: "868814127084",
+  appId: "1:868814127084:web:7de201588f19f0c88ccac9",
+  measurementId: "G-HZ3ENE2VKY"
 };
+
+export const environment = {
+  production: true,
+  firebaseConfig: firebaseConfig,
+  bloquearAutoLogin: false,
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
